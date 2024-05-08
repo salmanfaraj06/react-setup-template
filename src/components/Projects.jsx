74b1d7d7c8309bfbd376d8ef0acbd096cf3bca59
hashimkalam <hashim.20211291@iit.ac.lk>
2024-05-08 13:46:34 +0530

    projects component adjusted

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -5,56 +5,57 @@ import { Heading } from "./Heading";
 import { Button } from "./Button";
 
 const data = [
-    { 
-        portfolio: "images/img_rectangle_6.png",
-        title: "Portfolio design",
-        description: "UI design - User research - webflow develop",
-        link: "#"
-    },
-    { 
-      portfolio: "images/img_rectangle_6.png",
-      title: "Portfolio design",
-      description: "UI design - User research - webflow develop",
-      link: "#"
-  },
-  { 
+  {
     portfolio: "images/img_rectangle_6.png",
     title: "Portfolio design",
     description: "UI design - User research - webflow develop",
-    link: "#"
-},
-{ 
-  portfolio: "images/img_rectangle_6.png",
-  title: "Portfolio design",
-  description: "UI design - User research - webflow develop",
-  link: "#"
-},
-    // ... add more projects here
+    link: "#",
+  },
+  {
+    portfolio: "images/img_rectangle_6.png",
+    title: "Portfolio design",
+    description: "UI design - User research - webflow develop",
+    link: "#",
+  },
+  {
+    portfolio: "images/img_rectangle_6.png",
+    title: "Portfolio design",
+    description: "UI design - User research - webflow develop",
+    link: "#",
+  },
+  {
+    portfolio: "images/img_rectangle_6.png",
+    title: "Portfolio design",
+    description: "UI design - User research - webflow develop",
+    link: "#",
+  },
+  // ... add more projects here
 ];
 
 const ProjectCard = ({ portfolio, title, description, link }) => (
   <div className="relative w-full">
     <div className="relative w-full h-[560px] bg-white-A700_01 overflow-hidden md:h-auto">
-  <div className="absolute inset-0 bg-[url(/public/images/img_group_7.svg)] bg-no-repeat bg-center bg-cover" />
-      <div className="flex flex-col justify-between h-full p-8 md:p-5">
-        <div>
-          <Img
-            src={portfolio}
-            alt="portfolio"
-            className="w-full h-auto max-h-[265px] md:max-h-[200px] mb-4"
-          />
-          <div>
-            <Heading as="h2">{title}</Heading>
-            <Text as="p" className="my-3 w-[75%]">
-              {description}
-            </Text>
+      <div className="absolute inset-0 bg-[url(/public/images/img_group_7.svg)] bg-no-repeat bg-center bg-cover" />
+      <div className="flex flex-col justify-between h-full p-10 md:p-5">
+        <Img
+          src={portfolio}
+          alt="portfolio"
+          className="w-full h-auto max-h-[265px] md:max-h-[200px] mb-4"
+        />
+
+        <div className="h-full">
+          <div className="flex items-center justify-between ">
+            <Heading as="h2">{title}</Heading>{" "}
+            <Button size="xs" shape="square" className="w-10 h-10">
+              <a href={link}>
+                <Img src="images/img_arrow_left.svg" alt="arrow" />
+              </a>
+            </Button>
           </div>
+          <Text as="p" className="my-3 w-[75%]">
+            {description}
+          </Text>
         </div>
-        <Button size="xs" shape="square" className="w-10 h-10">
-          <a href={link}>
-            <Img src="images/img_arrow_left.svg" alt="arrow" />
-          </a>
-        </Button>
       </div>
     </div>
   </div>
@@ -73,7 +74,7 @@ function Projects() {
           className="h-4 w-full"
         />
       </div>
-      <div className="grid grid-cols-2 gap-10 md:grid-cols-2 md:gap-6">
+      <div className="grid grid-cols-2 gap-10 md:grid-cols-1 md:gap-6">
         {data.map((project, index) => (
           <ProjectCard
             key={"project" + index}
