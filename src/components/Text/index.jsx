b3dc91f3075c481f8fa682d4e7331bb67b3c0d7c
salmanfaraj06 <salman.faraj06@gmail.com>
2024-05-13 23:59:09 +0530

    chore: Update font size in ProjectsArea and Text components

--- src/components/Text/index.jsx
+++ src/components/Text/index.jsx
@@ -6,7 +6,7 @@ const sizes = {
   s: "text-[25px] font-medium md:text-[23px] sm:text-[21px]",
   "2xl": "text-5xl font-medium md:text-[44px] sm:text-[38px]",
   "3xl": "text-[72px] font-medium  md:text-[46px]",
-  xl: "text-4xl font-bold md:text-[30px] sm:text-[18px]",
+  xl: "text-4xl font-bold md:text-[22px] sm:text-[18px]",
   md: "text-[28px] font-medium md:text-[26px] sm:text-[24px]",
   mypro :" text-center text-[72px] font-medium  md:text-[46px]"
 };
