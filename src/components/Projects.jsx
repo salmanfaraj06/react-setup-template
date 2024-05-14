20b265d34d8055ccb51f26e22f1264ccb09f0925
rukinish <rukaiyanishfan@gmail.com>
2024-05-14 22:06:49 +0530

    Update Projects.jsx

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -8,38 +8,41 @@ const data = [
   {
     portfolio: "images/thumbnails/movematee.png",
     title: "moveMate",
-    description: "UI design - User research",
+    description:
+      "A case study on the mobile application 'moveMate'" +
+      "\n moveMate is a gamified solution designed to transform stroke recovery into an engaging and empowering journey." +
+      "\n Also avaiable as a web application",
     link: "https://www.behance.net/gallery/198496933/moveMate",
   },
   {
     portfolio: "images/thumbnails/IX-23.png",
     title: "IX-23 designathon",
-    description: "UI design - User research",
+    description: "UI design - Apple Vision Pro",
     link: "https://www.figma.com/file/QmbiGJuGQ8PzSPz8PZUsI7/Design100?type=design&mode=design&t=y6RBPTOJ31fTNkGV-1",
   },
 
   {
     portfolio: "images/thumbnails/beepure.png",
     title: "BeePure",
-    description: "UI design - User research",
+    description: "UI design - Mobile App",
     link: "https://www.figma.com/file/exWltRLkMIwUx78BztqYJ7/Honey-App?type=design&node-id=0%3A1&mode=design&t=y6RBPTOJ31fTNkGV-1",
   },
   {
     portfolio: "images/thumbnails/Rescufy.png",
     title: "Rescufy",
-    description: "UI design - User research",
+    description: "UI design - Mobile App",
     link: "https://www.figma.com/file/FQMrevNmTpnGfo7YLAZnE0/rescufy?type=design&node-id=1%3A131&mode=design&t=y6RBPTOJ31fTNkGV-1",
   },
   {
     portfolio: "images/thumbnails/ticketMate.png",
     title: "ticketMate",
-    description: "UI design - User research",
+    description: "UI design - Mobile App",
     link: "https://www.figma.com/file/W9XHPCuDH6nZNaw7YfIBhH/Benchmark?type=design&node-id=0-1&mode=design&t=y6RBPTOJ31fTNkGV-0",
   },
   {
     portfolio: "images/thumbnails/spaceOdessey.png",
     title: "Space Odessey",
-    description: "UI design - User research",
+    description: "UI design - Mobile App",
     link: "https://www.figma.com/file/m9o26LfpKlj3qZOD6xmxIa/dev100?type=design&mode=design&t=y6RBPTOJ31fTNkGV-0",
   },
   // ... add more projects here
